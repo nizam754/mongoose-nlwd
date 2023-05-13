fa70e4473f0d3c97e8d4da71630680ad6bb4ce30
nizam754 <m.nizam754@gmail.com>
2023-05-13 13:32:35 +0600

    6-5: Create Model using Interface , Schema and Insert Data

--- src/app.ts
+++ src/app.ts
@@ -100,9 +100,35 @@ const userSchema = new Schema<IUser>({
         required: true
     }
   });
+
+  //Create a Model.
+
+const User = model<IUser>("User", userSchema);
+
+const createUserTODB=async ()=> {
+    const user = new User({
+        id: "778",
+        role: "student",
+        password: 'mypassword',
+        name: {
+            firstName: "M.",
+            middleName: "Nizam",
+            lastName: "Babu"
+        },
+        gender: "male",
+        email: "abc@gmail.com",
+        contactNo: "01265896658",
+        emergencyContactNo:"01236548965",
+        presentAddress:  "Dhaka",
+        permanentAddress: "KSA"
+    });
+      await user.save();
+      console.log(user);
+    };
+
+    createUserTODB();
+
 });
 
-
-  
 
 export default app;
